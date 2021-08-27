34901b7d8b4cea46ff7273f0bbc8e6d40c4802bd
ridmik <poran.cse@gmail.com>
2021-08-27 21:40:51 +0600

    created employee model

--- EmplyeeApp/models.py
+++ EmplyeeApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Department(models.Model):
+
+    departmentId = models.AutoField(primary_key=True)
+    departmentName = models.CharField(max_length=200)
+
+class Employee(models.Model):
+
+    employeeId = models.AutoField(primary_key=True)
+    employeeName = models.CharField(max_length=200)
+    dateOfJoining = models.DateField(default=datetime.now,blank=True)
+    departmentName = models.CharField(max_length=200)
+    photoFileUrl = models.FileField(upload_to='image')
